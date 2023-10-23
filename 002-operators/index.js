32f4aaf0f337f582beaaec0a02efdad2e0c987c2
03Jesus <saavedraj@utb.edu.co>
2023-10-23 15:48:11 -0500

    learning operators

---
+++ 002-operators/index.js
@@ -0,0 +1,36 @@
+/*
+Assignment operators
+x = y -> x = y
+x += y -> x = x + y
+x -= y -> x = x - y
+x *= y -> x = x * y
+x /= y -> x = x / y
+x %= y -> x = x % y (remainder of x/y)
+x **= y -> x = x ** y
+x <<= y -> x = x << y (left shift)
+x >>= y -> x = x >> y (right shift)
+*/
+document.write(`<h1>Assignment operators</h1><br>`);
+document.write(`let num = 10;<br>`);
+document.write(`num += 10;<br>`);
+let num = 10;
+num += 10;
+document.write("num = " + num + "<br>");
+
+/*
+Arithmetic operators
++ -> Addition
+- -> Subtraction
+* -> Multiplication
+/ -> Division
+% -> Modulus (remainder of x/y)
+** -> Exponentiation
+++ -> Increment
+-- -> Decrement
+*/
+document.write(`<h1>Arithmetic operators</h1><br>`);
+document.write(`let num = 20;<br>`);
+document.write(`num++;<br>`);
+num = 20;
+num++;
+document.write("num = " + num + "<br>");
