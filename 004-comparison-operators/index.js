5518b2e9dd062dfb95676eae3582d02eab432fcf
03Jesus <saavedraj@utb.edu.co>
2023-10-24 10:53:05 -0500

    learning comparision operators

---
+++ 004-comparison-operators/index.js
@@ -0,0 +1,21 @@
+/*
+Comparison Operators
+
+== equal to
+=== equal value and equal type
+!= not equal
+!== not equal value or not equal type
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+Returns true or false if the condition is met
+*/
+
+let num1 = 23;
+let num2 = 13;
+document.write(`<h1>Comparison Operators</h1>`);
+document.write(`<p>num1 = ${num1}</p>`);
+document.write(`<p>num2 = ${num2}</p>`);
+document.write(`<p>num1 == num2: ${num1 == num2}</p>`);
